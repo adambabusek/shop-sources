584117b85f3e764077d9e48b6ed5590e0d6bc1c9
adambabusek <adam.babusek@gmail.com>
2016-11-06 19:11:44 +0100

    added thymeleaf engine and login html

--- shop-admin/src/main/java/com/shop/admin/controllers/AppController.java
+++ shop-admin/src/main/java/com/shop/admin/controllers/AppController.java
@@ -4,11 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
 public class AppController {
 
-	@RequestMapping("")
-	public String defaultView() {		
-		return "app";
+	@RequestMapping("/login")
+	public String loginView() {		
+		return "login";
 	}
 }
